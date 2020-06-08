53dcc69e9c26e4808d64ea7fd414ae5d0601b782
nhhuyen <65443733+nhhuyen@users.noreply.github.com>
2020-06-08 20:46:20 +0700

    second commit

--- login/dx.php
+++
@@ -1,7 +0,0 @@
-<?php session_start(); 
- 
-if (isset($_SESSION['nd_taikhoan'])){
-    unset($_SESSION['nd_taikhoan']); // xóa session login
-}
-?>
-<a href="tt.php">Trang Chủ</a>
--- php/Connect.php
+++
@@ -1,6 +0,0 @@
-
-<?php
-
-$conn = mysqli_connect("localhost", "root", "", "tendata");
- mysqli_query($conn,"SET NAMES 'utf8'");
-?>
